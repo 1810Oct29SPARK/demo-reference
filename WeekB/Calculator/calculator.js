2049a22145dc3716f40a3735281480de179fcd28
Emily Higgins <emilyrchiggins@gmail.com>
2018-11-06 15:04:08 -0500

    adding calculate function

--- WeekB/Calculator/calculator.js
+++ WeekB/Calculator/calculator.js
@@ -3,3 +3,20 @@
 window.onload = function(){
     console.log("hello world");
 }
+
+function calculate(num1, num2, operation){
+    let result=NaN;
+    if (operation === "add"){
+        result = num1+num2;
+    }
+    if (operation === "subtract"){
+        result = num1-num2;
+    }
+    if (operation === "divide"){
+        result = num1/num2;
+    }
+    if (operation === "multiply"){
+        result = num1*num2;
+    }
+    return result;
+}
