36b881f36e893b6f33205329c0e08ded8da208a7
Emily Higgins <emilyrchiggins@gmail.com>
2018-11-15 16:59:33 -0500

    more constructors

--- WeekC/Thursday/src/Driver.java
+++ WeekC/Thursday/src/Driver.java
@@ -3,11 +3,18 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		System.out.println("this is the point of entry into my program");
+		
 		//make a car object with no set fields
 		//declare the variable carOne and assign a new Car to it
 		Car carOne = new Car();
 		//toString is a method inherited from Object
 		System.out.println("carOne: "+carOne.toString());
+		
+		//make a car object with instance variables set in the constructor
+		//declare the variable CarTwo and assign an new Car to it
+		Car carTwo = new Car(1972,"DeLorean","DMC");
+		//check out the toString result
+		System.out.println("carTwo: "+carTwo.toString());
 	}
 
 }
