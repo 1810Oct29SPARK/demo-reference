d7afee5290202c1c7ea135180fda9fa5cb74b91a
Emily Higgins <emilyrchiggins@gmail.com>
2018-12-14 16:29:32 -0500

    bears

---
+++ ServletDemo/src/main/java/com/revature/servlet/BearServlet.java
@@ -0,0 +1,43 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.revature.service.BearService;
+
+/**
+ * Servlet implementation class BearServlet
+ */
+public class BearServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private BearService bearService = new BearService();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BearServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append(bearService.getBearsJSON());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
